2d79e88edc0de572fcb20023fcf71c79865172ad
Jiawei Liang <wilsonjw.liang@mail.utoronto.ca>
2025-09-08 14:52:31 -0400

    update if

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,12 +2,22 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+        extracted();
+    }
+
+    private static void extracted() {
+        int count = multiples();
+        System.out.println("There are " + count + " multiples of 3 and 5.");
+    }
+
+    private static int multiples() {
         int count = 0;
 
         for(int i = 1; i < 1000; i++) {
-            if(i % 3 == 0 || i % 5 == 0);
-            count++;
+            if(i % 3 == 0 || i % 5 == 0){
+                count++;
+            }
         }
-        System.out.println("There are" + count + " multiples of 3 and 5.");
+        return count;
     }
 }
